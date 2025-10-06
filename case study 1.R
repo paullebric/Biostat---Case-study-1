c89dbd84b07c46cda5477cb5f3a7f154fa76c59e
paullebric <paullebric@gmail.com>
2025-10-06 11:04:00 +0200

    Update case study 1.R

--- case study 1.R
+++ case study 1.R
@@ -1,1 +1,39 @@
-lalalala
+library(ggplot2)
+library(readxl)
+Data <- read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Data")
+Summary <- read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Summary")
+Info <- read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Info")
+Codebook <- read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Codebook")
+
+All_Data <- list(
+  Data = Data,
+  Summary = Summary,
+  Info = Info,
+  Codebook = Codebook
+)
+
+#$$$1 Data Exploration
+lapply(All_Data,head)
+lapply(All_Data,dim)
+lapply(All_Data,sum(is.na))
+lapply(All_Data,str)
+
+
+
+#$$$2 Assumption checks
+
+
+
+#$$$3 Test or Model Selection
+
+
+
+#$$$4 Results
+
+
+
+#$$$5 Post-Hoc Analysis
+
+
+
+#$$$6 
