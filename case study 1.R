3f9832490f8966f85376af5fb97a4fd7db6921c4
paullebric <paullebric@gmail.com>
2025-10-06 14:27:37 +0200

    Update case study 1.R

--- case study 1.R
+++ case study 1.R
@@ -5,6 +5,7 @@ Data <- read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Data"
 Summary <- read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Summary")
 Info <- read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Info")
 Codebook <- read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Codebook")
+
 #I)DATA IMPORTATION
 All_Data <- list(
   Data = Data,
@@ -18,11 +19,14 @@ lapply(All_Data,head)
 lapply(All_Data,dim)
 lapply(All_Data, function(x){str(x); head(x)})
 lapply(All_Data,str)
-desc1 <- tapply(All_Data$Data, list(All_Data$Data), function(x) c(n=length(x),
+n_all <- nrow(Data)
+n_na <- sum(is.na(??$??))
+tab <- with(??, table(??, ??))
+desc1 <- tapply(??$??, list(??$??), function(x) c(n=length(x),
                                                   mean=mean(x,na.rm=TRUE), sd=sd(x,na.rm=TRUE)))
-desc2 <- tapply(All_Data$Data, list(All_Data$Data, All_Data$Data), function(x) c(n=length(x),
+desc2 <- tapply(??$??, list(??$??, ??$??), function(x) c(n=length(x),
                                                          mean=mean(x,na.rm=TRUE), sd=sd(x,na.rm=TRUE)))
-
+n_all; n_na; tab; desc1; desc2
 
 #III) ASSUMPTION CHECK / CTL
 spec0 <- ??(?? ~ ?? * ??, ??a = ??)
