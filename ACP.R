3de0a3bb76ae35b233b027f257c4f232017d6a7d
paullebric <paullebric@gmail.com>
2025-10-07 14:13:56 +0200

    Update ACP.R

--- ACP.R
+++ ACP.R
@@ -1,1 +1,8 @@
 library(MASS)
+library(factoextra)
+library(ggplot2)
+library(readxl)
+
+data <- data.frame(read_excel("Biostatistics 5A 1st Case Study dataset.xlsx",sheet = "Data"))
+dim(data)
+str(data)
